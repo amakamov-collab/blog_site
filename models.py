cfd6597b9e68ff6dc0e0b9698c9e1a89b1f501cc
❤️ Nigina ❤️ <amakamov@gmail.com>
2026-01-04 12:35:56 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,39 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+class Meta:
+    ordering = ('-publish',)
+
+   def __str__(self):
+       return self.title
+
+   objects = models.Manager()
+   published = PublishedManager()
+
+   # def get_absolute_url(self):
+   #     return reverse("")
+
+posts = Post.published.filter(status='published')
+p_post = Post.published.all()
+
---
+++ views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import get_object_or_404
+
+from .models import Post
+from django.views.generic import ListView,
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    ordering = '-published'
+    paginate_by = 5
+    template_name = "blog/post/list.html"
+
+def post_detail(request,year, month, day, post,slug):
+    post = get_object_or_404(Post,slug=slug,status='published',publish__year=year,publish__month=month,publish__day=day)
+    return render(request, 'blog/post/detail.html',{'post':post})
+
+
+
